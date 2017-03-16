61b580d108696f49ca9b11c3fb10679dc57fffe7
LuisMiguelRodriguez <tanukitek@gmail.com>
2017-03-16 13:02:05 -0400

    Gulp Automation

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+    cssVars = require('postcss-simple-vars'),
+    nested = require('postcss-nested');
+
+
+gulp.task('default', function(){
+  console.log("You just created you first gulp task!");
+})
+
+gulp.task('html', function (){
+  console.log('Html Task running now...');
+});
+
+gulp.task('styles', function (){
+  return gulp.src('./app/assets/styles/styles.css')
+  .pipe(postcss([nested, cssVars,autoprefixer]))
+  .pipe(gulp.dest('./app/assets/temp/styles'));
+});
+
+// This will trigger several task that are listen within
+
+gulp.task('watch', function(){
+
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  });
+
+});

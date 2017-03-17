1e45ba3d80689dac4f6a25464ec1a19c9c5f86c6
LuisMiguelRodriguez <tanukitek@gmail.com>
2017-03-17 18:51:25 -0400

    Modulized gulp tasks

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+  //loads browser
+  browserSync.init({
+    server: {
+      notify: false,
+      baseDir: "app"
+    }
+  });
+
+  watch('./app/index.html', function(){
+    // reloads the browser when index.html gets saved
+    browserSync.reload();
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('cssInject');
+  });
+
+});
+
+// before this task is run it will run the second argument
+// which is an array of dependecy task to run
+gulp.task('cssInject', ['styles'], function(){
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
